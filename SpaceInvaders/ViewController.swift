ecff896efbc59ddab05ac3766225688c9efadecf
Admin <admin@Mac-Admin.local>
2021-03-06 13:30:37 +0300

    Initial Commit

---
+++ SpaceInvaders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceInvaders
+//
+//  Created by Admin on 06.03.2021.
+//  Copyright © 2021 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
